8fb54c04940f57e40a77eafcec2f3339c24cafd1
ELghazX <2309106041@student.unmul.ac.id>
2024-11-12 16:09:36 +0800

    initial commit

---
+++ classes/users.php
@@ -0,0 +1,69 @@
+<?php
+class User
+{
+    private $conn;
+    private $table_name = "users";
+
+    public $username;
+    public $password;
+    public $email;
+    public $role;
+
+    public function __construct($db)
+    {
+        $this->conn = $db;
+    }
+
+    public function isUsernameExists() {
+        $query = "SELECT COUNT(*) FROM " . $this->table_name . " WHERE usn = :username";
+        $stmt = $this->conn->prepare($query);
+        $stmt->bindParam(":username", $this->username);
+        $stmt->execute();
+        return $stmt->fetchColumn() > 0;
+    }
+
+    public function register()
+    {
+        if ($this->isUsernameExists()) {
+            return "Username sudah terdaftar. Gunakan username lain.";
+        }
+        $query = "INSERT INTO " . $this->table_name . " (usn, email, pw, role) VALUES (:username, :email, :password, :role)";
+
+        $stmt = $this->conn->prepare($query);
+
+        $stmt->bindParam(":username", $this->username);
+        $stmt->bindParam(":email", $this->email);
+        $stmt->bindParam(":password", $this->password);
+        $stmt->bindParam(":role", $this->role);
+
+        if ($stmt->execute()) {
+            return true;
+        }
+        return false;
+    }
+
+    public function login() {
+        if (isset($_POST['username']) && isset($_POST['password'])) {
+            $input = $_POST['username'];  
+            $password = $_POST['password'];  
+            
+            $query = "SELECT * FROM " . $this->table_name . " WHERE (usn = :input OR email = :input)";
+            $stmt = $this->conn->prepare($query);
+            $stmt->bindParam(":input", $input);
+            $stmt->execute();
+
+            if ($stmt->rowCount() > 0) {
+                $row = $stmt->fetch(PDO::FETCH_ASSOC);
+
+                if (password_verify($password, $row['pw'])) {
+                    $this->username = $row['usn'];
+                    $this->role = $row['role'];
+                    $this->email = $row['email'];
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    
+}
---
+++ newrelease.php
@@ -0,0 +1,196 @@
+<html lang="id">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>
+        New Releases
+    </title>
+
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" rel="stylesheet" />
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet" />
+    <link rel="stylesheet" href="newrelease.css">
+    <link rel="icon" type="image/jpeg" href="img/icon.ico">
+    
+</head>
+
+<body>
+    <div class="container">
+        <div class="header">
+            <img alt="Logo"  src="image/logo.png"/>
+            <div class="nav">
+                <a href="#">
+                    Best Sellers
+                </a>
+                <a href="#">
+                    New Releases
+                </a>
+                <a href="#">
+                    Collections
+                </a>
+                <a href="#">
+                    About
+                </a>
+            </div>
+            <div class="search-bar">
+                <input placeholder="Search" type="text" />
+                <i class="fas fa-search">
+                </i>
+            </div>
+            <div class="icons">
+                <img src="image/tas.png" alt="Cart Icon">
+                <img src="image/akun.png" alt="akun Icon">
+            </div>
+        </div>
+        <div class="filter-sort-container">
+            <div class="filter">
+                <button style="padding: 10px; border-radius: 5px; border: none; background-color: #fff; color: #000;">
+                    Filter
+                </button>
+                <div class="filter-box">
+                    <h3>
+                        Filter
+                    </h3>
+                    <div>
+                        <label for="max-price">
+                            Price Range
+                        </label>
+                        <input id="max-price" placeholder="Maximum" type="text" />
+                        <input id="min-price" placeholder="Minimum" type="text" />
+                    </div>
+                    <div class="brands">
+                        <h4>
+                            Brands
+                        </h4>
+                        <label>
+                            <input type="checkbox" />
+                            Kanky
+                        </label>
+                        <label>
+                            <input type="checkbox" />
+                            Compass
+                        </label>
+                        <label>
+                            <input type="checkbox" />
+                            Marque
+                        </label>
+                        <label>
+                            <input type="checkbox" />
+                            Ardiles
+                        </label>
+                        <label>
+                            <input type="checkbox" />
+                            Ventela
+                        </label>
+                    </div>
+                </div>
+            </div>
+            <div class="sort-box">
+                <button style="padding: 10px; border-radius: 5px; border: none; background-color: #fff; color: #000;">
+                    Sort by Price
+                </button>
+                <div class="sort-menu">
+                    <label>
+                        <i class="fas fa-sort-amount-up"></i> Ascending
+                    </label>
+                    <label>
+                        <i class="fas fa-sort-amount-down"></i> Descending
+                    </label>
+                </div>
+            </div>
+        </div>
+        <div class="products">
+            <div class="product-row">
+                <div class="product-card">
+                    <img alt="Fender Heritage White" height="200" src="#" width="200" />
+                    <h4>
+                        Fender Heritage
+                    </h4>
+                    <p>
+                        White
+                    </p>
+                    <p>
+                        RP 499.000
+                    </p>
+                </div>
+                <div class="product-card">
+                    <img alt="EXC 02 KANKY White" height="200" src="#" width="200" />
+                    <h4>
+                        EXC 02
+                    </h4>
+                    <p>
+                        KANKY
+                    </p>
+                    <p>
+                        White
+                    </p>
+                    <p>
+                        RP 350.000
+                    </p>
+                </div>
+                <div class="product-card">
+                    <img alt="Dr Tirta KANKY White" height="200" src="#" width="200" />
+                    <h4>
+                        Dr Tirta
+                    </h4>
+                    <p>
+                        KANKY
+                    </p>
+                    <p>
+                        White
+                    </p>
+                    <p>
+                        RP 389.000
+                    </p>
+                </div>
+            </div>
+        </div>
+        <div class="pagination-container">
+            <p>Showing page 3 of 3</p>
+            <div class="pagination">
+                <a href="#"><i class="fas fa-chevron-left"></i></a>
+                <a href="#">1</a>
+                <a href="#">2</a>
+                <a href="#">3</a>
+                <a href="#"><i class="fas fa-chevron-right"></i></a>
+            </div>
+        </div>
+
+        <div class="footer">
+            <div class="footer-section">
+                <h3>Express</h3>
+                <p>Express is an e-commerce website sought to introduce local Indonesian shoe products</p>
+            </div>
+            <div class="footer-section">
+                <h3>Products</h3>
+                <p>Sneakers</p>
+                <p>Dunks</p>
+                <p>Hightops</p>
+            </div>
+            <div class="footer-section">
+                <h3>Useful Links</h3>
+                <p>New Releases</p>
+                <p>Best Sellers</p>
+                <p>Collections</p>
+            </div>
+            <div class="footer-section">
+                <h3>Contact Us</h3>
+                <div class="social-icons">
+                    <a href="#"><i class="fas fa-envelope"></i></a>
+                    <a href="#"><i class="fab fa-tiktok"></i></a>
+                    <a href="#"><i class="fab fa-instagram"></i></a>
+                </div>
+                <div class="social-icons">
+                    <a href="#"><i class="fab fa-facebook"></i></a>
+                    <a href="#"><i class="fab fa-youtube"></i></a>
+                </div>
+            </div>
+        </div>
+
+        <div class="copyright">
+            &copy; 2024 copyright
+        </div>
+    </div>
+</body>
+
+</html>
