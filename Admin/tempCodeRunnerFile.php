b27ae08a6d87a2ba0abed0a97cebbd87e0e304f5
ELghazX <2309106041@student.unmul.ac.id>
2024-11-18 20:53:23 +0800

    chore(admin): removes tempCodeRunnerFile.php

--- Admin/tempCodeRunnerFile.php
+++
@@ -1,9 +0,0 @@
-<?php
-<div class="pagination">
-                        <span>Number of pages</span>    
-                        <a href="#">&laquo;</a>
-                        <a href="#">1</a>
-                        <a href="#">2</a>
-                        <a href="#">3</a>
-                        <a href="#">&raquo;</a>
-                    </div>
