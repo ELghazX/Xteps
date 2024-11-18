3ddbbc964394f5652945053617af4d0a69c4903a
Muhammad Ghazali <152017206+ELghazX@users.noreply.github.com>
2024-11-19 07:52:17 +0800

    (db) change username db

--- config/database.php
+++ config/database.php
@@ -2,7 +2,7 @@
 class Database
 {
     private $host = "localhost";
-    private $db_name = "nama_database";
+    private $db_name = "express";
     private $username = "root";
     private $password = "";
     public $conn;
